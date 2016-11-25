25bb69f558e965c63907b2ddc8e37b76d0d0d49a
nahumsin <nahumaldonado@gmail.com>
2016-11-25 00:34:00 -0600

    Borrar Cuenta Con Confirmación

--- app/src/main/java/com/example/nahumsin/donadif/ModificarCuenta.java
+++ app/src/main/java/com/example/nahumsin/donadif/ModificarCuenta.java
@@ -1,24 +1,30 @@
 package com.example.nahumsin.donadif;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by nahumsin on 24/11/16.
  */
 
-public class ModificarCuenta extends AppCompatActivity implements View.OnClickListener{
+public class ModificarCuenta extends AppCompatActivity implements View.OnClickListener {
     private EditText textName;
     private EditText email;
     private EditText password;
     private Button update;
     private Button delete;
     private ConectionDB db;
+    private Cuenta cuenta;
 
     String id;
 
@@ -31,7 +37,7 @@ public class ModificarCuenta extends AppCompatActivity implements View.OnClickLi
 
         id = intent.getStringExtra("id_usuario");
         db = new ConectionDB(this);
-
+        cuenta = db.getCuenta(id);
         textName = (EditText) findViewById(R.id.txtUsuario);
         email = (EditText) findViewById(R.id.txtEmail);
         password = (EditText) findViewById(R.id.txtPass);
@@ -44,18 +50,90 @@ public class ModificarCuenta extends AppCompatActivity implements View.OnClickLi
 
         mostrarCuenta();
     }
+
     @Override
     public void onClick(View view) {
-        if(view == delete){
-            db.eliminarCuenta(id);
-            Intent intent = new Intent(ModificarCuenta.this,Login.class);
-            startActivity(intent);
+        if (view == delete) {
+            verificarUsuario();
         }
     }
 
-    public void mostrarCuenta(){
-        Cuenta cuenta = db.getCuenta(id);
+    public void mostrarCuenta() {
+
         textName.setText(cuenta.getNombreUsuario());
         email.setText(cuenta.getCorreo());
+    }
+
+    private void verificarUsuario() {
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.prompt, null);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ModificarCuenta.this);
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView
+                .findViewById(R.id.editTextDialogUserInput);
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setNegativeButton("Eliminar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int ide) {
+                                /** DO THE METHOD HERE WHEN PROCEED IS CLICKED*/
+                                String user_text = (userInput.getText()).toString();
+
+                                /** CHECK FOR USER'S INPUT **/
+                                if (user_text.equals(cuenta.getContrasena())) {
+                                    asegurarAccion();
+                                } else {
+                                    String message = "La contraseña introducida es incorrecta." + " \n \n" + "Por favor vuelva a intentar!";
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(ModificarCuenta.this);
+                                    builder.setTitle("Error");
+                                    builder.setMessage(message);
+                                    builder.setPositiveButton("Cancelar", null);
+                                    builder.setNegativeButton("Reintentar", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            verificarUsuario();
+                                        }
+                                    });
+                                    builder.create().show();
+                                }
+                            }
+                        })
+                .setPositiveButton("Cancelar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        }
+                );
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show it
+        alertDialog.show();
+    }
+    private void asegurarAccion() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ModificarCuenta.this);
+        alertDialogBuilder.setMessage("¿Estás seguro de que deseas realizar esta acción?");
+
+        alertDialogBuilder.setPositiveButton("Si",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        db.eliminarCuenta(id);
+                        Intent intent = new Intent(ModificarCuenta.this,Login.class);
+                        startActivity(intent);
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
